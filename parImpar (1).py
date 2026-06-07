a87e85bb6e693631252438bfadada0ab9a56651f
idgar1996 <geanschneider1996@gmail.com>
2026-06-07 19:57:12 -0300

    atividades

--- parImpar (1).py
+++
@@ -1,9 +0,0 @@
-def imparPar(numero):
-
-    if numero % 2 == 0:
-        print(f"O número {numero} é par!")
-    else:
-        print(f"O número {numero} é impar!")
-
-valor = float(input("Digite um número: "))
-imparPar(valor)
